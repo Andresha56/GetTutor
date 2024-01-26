38eaecbba6a3cd897d21b47c32fd7046d147bb4a
Andresha56 <rinkyshaw65479@gmail.com>
2024-01-26 12:30:55 +0530

    added validation to signup form

--- client/src/components/signup/Signup.jsx
+++ client/src/components/signup/Signup.jsx
@@ -1,7 +1,7 @@
 import "./Signup.css";
-import { Container, Box, Button, Stack, } from '@mui/material'
-import React from 'react'
-import signupImg from "../../image/signupImg.png"
+import { Container, Box, Button, Stack, } from '@mui/material';
+import React, { useEffect } from 'react';
+import signupImg from "../../image/signupImg.png";
 import { Link, useLocation } from 'react-router-dom';
 import { useState } from "react";
 
@@ -11,23 +11,57 @@ const initialValues = {
   email: "",
   password: ""
 }
+
 function Signup() {
   const { state } = useLocation();
   // -----states-----
   const [values, setValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [isNavigate,setIsNavigate]=useState(false);
   // ---handelFormClick-----
 
   const handelFormClick = (e) => {
-    const {name,value}=e.target;
+    const { name, value } = e.target;
     setValues({
       ...values,
       [name]: value,
     });
   }
   // -----handel---form----data--after---submit----
-  const handelFormSubmit=(e)=>{
+  const handelFormSubmit = (e) => {
     e.preventDefault();
-    console.log(values)
+    // Check for errors and update the error state
+    setFormErrors((prevErrors) => {
+      const newErrors = checkValidation(values);
+      console.log(newErrors);
+      return newErrors;
+    });
+    setIsSubmit(true);
+  }
+  // ----check--form--fiellds---and---navigate--accordingly ---
+  useEffect(()=>{
+    if (isSubmit && Object.keys(formErrors).length === 0)setIsNavigate(true);
+  },[formErrors,isSubmit])
+  // ------vaildate---function-----
+  const checkValidation = (formValues) => {
+    const errors = {};
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const { name, email, password } = formValues;
+    if (!name) {
+      errors.name = "name required!"
+    } else if (name.length < 3) {
+      errors.name = "name must be at least 3 characters long.";
+    };
+    if (!email) errors.email = "email required!"
+    else if (!emailRegex.test(email)) {
+      errors.email = "Invalid email address!";
+    };
+    if (!password) errors.password = "password required!"
+    else if (password.length < 7) {
+      errors.password = "Password must be at least 8 characters long.";
+    };
+    return errors;
   }
   return (
     <>
@@ -38,14 +72,16 @@ function Signup() {
               <form method="post" onSubmit={handelFormSubmit} >
                 <label htmlFor="name" >Enter name</label>
                 <input type="text" value={values.name} onChange={handelFormClick} placeholder="name" name="name" id="name" />
+                {formErrors?.name&& <p>{formErrors?.name}</p> }
 
                 <label htmlFor="email" >Enter email</label>
                 <input type="email" value={values.email} onChange={handelFormClick} placeholder="example@gmail.com" name="email" id="email" />
-
+                {formErrors?.email&& <p>{formErrors?.email}</p> }
                 <label htmlFor="password">Enter password</label>
                 <input type="password" onChange={handelFormClick} value={values.password} placeholder="1234#$abM" name="password" id="password" />
+                {formErrors?.password&& <p>{formErrors?.password}</p> }
                 <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
-                <Button type="submit" >Sign Up</Button><hr/><Button>Sign up with Google</Button>
+                  <Button type="submit" >Sign Up</Button><hr /><Button>Sign up with Google</Button>
                 </Box>
 
                 <Stack flexDirection={"row"} justifyContent={"center"} mt={"20px"}>
